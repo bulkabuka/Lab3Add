4370a1d9b502826517d356bfa970bf8c0c6a0d3d
Erich Helvig <amaruelleof@gmail.com>
2023-09-09 08:07:20 +0300

    New profile screen and improved layout for the others

---
+++ app/src/main/java/com/leftbrained/lab3add/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.leftbrained.lab3add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_screen)
+    }
+}
